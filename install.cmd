d5a31f0b0efef28dee66cba711904658f1e06b70
realluke <49283004+reallukee@users.noreply.github.com>
2022-08-31 17:26:40 +0200

    Add files via upload

---
+++ install.cmd
@@ -0,0 +1,4 @@
+if exist C:\Users\%USERNAME%\AppData\Roaming\Dev-Cpp (
+    copy paperadark.syntax C:\Users\%USERNAME%\AppData\Roaming\Dev-Cpp
+    copy paperalight.syntax C:\Users\%USERNAME%\AppData\Roaming\Dev-Cpp
+)
---
+++ uninstall.cmd
@@ -0,0 +1,4 @@
+if exist C:\Users\%USERNAME%\AppData\Roaming\Dev-Cpp (
+    del C:\Users\%USERNAME%\AppData\Roaming\Dev-Cpp\paperadark.syntax
+    del C:\Users\%USERNAME%\AppData\Roaming\Dev-Cpp\paperalight.syntax
+)
